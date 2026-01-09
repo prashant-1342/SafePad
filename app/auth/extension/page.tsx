a63f16a80c188e1051f2cb07bef4a47a8d67a36a
Prashant Pal <prashantpal2024@gmail.com>
2026-01-09 22:32:18 +0530

    added dummy browser extension page

---
+++ app/auth/extension/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Extension(){
+    return (
+        <div className="flex bg-gray-500 items-center justify-center h-screen">
+            <h1 className="text-2xl font-bold italic">Comming Soon</h1>
+        </div>
+    );
+}
